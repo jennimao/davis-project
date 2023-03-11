86da0e378233a28d11f23447ff099058b28cf437
jennimao <jenmao16@gmail.com>
2023-03-11 06:24:38 -0500

    new angles

--- app/app.ino
+++ app/app.ino
@@ -22,68 +22,65 @@ unsigned long moveStartTime;
   
 
 void setup() {
-servo1.attach(3); // attaches the String1 on pin 3 to the servo object
-servo2.attach(5); // attaches the String2 on pin 5 to the servo object
-
-
-
-// Open communication with bluetooth and wait for port to open:
-Serial.begin(57600);
-
-
-Serial.println("Initiated");
-
-// set the data rate for the SoftwareSerial port
-mySerial.begin(9600);
-mySerial.println("Hello, world?");
-servo1.write(75);
-servo2.write(115);
-up = false;
+  servo1.attach(3); // attaches the String1 on pin 3 to the servo object
+  servo2.attach(5); // attaches the String2 on pin 5 to the servo object
+ 
+  // Open communication with bluetooth and wait for port to open:
+  Serial.begin(57600);
+  Serial.println("Initiated");
+  
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  mySerial.println("Hello, world?");
+  servo1.write(90);
+  servo2.write(100);
+  up = false;
 
 }
 
 void loop() {
 
-
-
-if (mySerial.available() ) // if recieve data
-{
-
-   data = mySerial.read(); // define data as the num recieved from BT
-   Serial.println(data);
-   if (data == 1)
-   { MOVING_TIME = 3000;}
-
-   if (data == 2)
-   { MOVING_TIME = 2000;}
-   
-   if (data == 3)
-   { MOVING_TIME = 1000;}
   
-   if (data == 4 && not up)
-   {
-    moveStartTime = millis(); // start moving
-    while (progress <= MOVING_TIME) {
-    progress = millis() - moveStartTime;
-    long angle = map(progress, 0, MOVING_TIME, 0, 90);
-    servo1.write(75 + angle); 
-    servo2.write(115 - angle); 
-  }
-  up = true;
-  progress = 0;
-   }
+  
+  if (mySerial.available() ) // if recieve data
+  {
+  
+     data = mySerial.read(); // define data as the num recieved from BT
+     Serial.println(data);
+     if (data == 1)
+     { MOVING_TIME = 3000;}
+  
+     if (data == 2)
+     { MOVING_TIME = 2000;}
+     
+     if (data == 3)
+     { MOVING_TIME = 1000;}
+    
+     if (data == 4 && not up)
+     {
+      moveStartTime = millis(); // start moving
+      while (progress <= MOVING_TIME) {
+      progress = millis() - moveStartTime;
+      long angle = map(progress, 0, MOVING_TIME, 0, 75);
+      servo1.write(90 + angle); 
+      servo2.write(100 - angle); 
+    }
+    up = true;
+    progress = 0;
+     }
+     
+     if (data == 5 && up)
+     {
+        moveStartTime = millis(); // start moving
+        while (progress <= MOVING_TIME) {
+          progress = millis() - moveStartTime;
+          long angle = map(progress, 0, MOVING_TIME, 75, 0);
+          servo1.write(90 + angle); 
+          servo2.write(100 - angle); 
+        }
+        progress = 0;
+        up= false;
+     }
    
-   if (data == 5 && up)
-   {
-    moveStartTime = millis(); // start moving
-    while (progress <= MOVING_TIME) {
-    progress = millis() - moveStartTime;
-    long angle = map(progress, 0, MOVING_TIME, 90, 0);
-    servo1.write(75 + angle); 
-    servo2.write(115 - angle); 
   }
-  progress = 0;
-  up= false;
-   }
-   
-}}
+}
